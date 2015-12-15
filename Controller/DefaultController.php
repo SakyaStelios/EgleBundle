01ff4c8ddb10fd5507e9eafacf471270332496aa
Giancarlo Ventura <sakya_stelios@hotmail.com>
2015-12-14 23:14:15 -0500

    Added annotation method

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -2,10 +2,16 @@
 
 namespace Valonde\EgleBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
+	/**
+	 * @Route("/", name="valonde_egle_homepage")
+	 * @Method("GET")
+	 */
     public function indexAction()
     {
         return $this->render('ValondeEgleBundle:Default:index.html.twig');
