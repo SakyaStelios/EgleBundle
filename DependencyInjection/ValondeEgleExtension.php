caedc68d297cb65942150dfdf9aae194433b5007
Giancarlo Ventura <sakya_stelios@hotmail.com>
2015-12-16 22:19:36 -0500

    Added set parameter

--- DependencyInjection/ValondeEgleExtension.php
+++ DependencyInjection/ValondeEgleExtension.php
@@ -22,6 +22,8 @@ class ValondeEgleExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
+        $container->setParameter('valondeegle.config', $config);
+
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
     }
